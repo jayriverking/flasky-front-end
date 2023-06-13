346e1e0ac964f66dae99854fcbc3434595018fcf
Jay King <jayriverking@gmail.com>
2023-06-13 11:11:32 -0400

    fixed errors

--- src/components/Crystal.js
+++ src/components/Crystal.js
@@ -1,19 +1,26 @@
 // React component files NEED to start with a capital letter!
 // (i.e., Crystal.js, NOT crystal.js)
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Crystal = ({ name, color, powers }) => {
+const Crystal = ({ name, color, powers, id }) => {
     // name="Amethyst";
     // color="Purple";
     // powers ="super powers";
     return (
-        <li>
+        <div>
             <h2>{name}</h2>
+            <p>{id}</p>
             <p>{color}</p>
             <p>{powers}</p>
             <button>Charge Crystal</button>
-        </li>
+        </div>
     );
 }
-
+Crystal.propTypes = {
+    name: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    powers: PropTypes.string.isRequired
+};
 export default Crystal;
+
--- src/components/CrystalList.js
+++ src/components/CrystalList.js
@@ -6,31 +6,36 @@ import Crystal from './Crystal';
 
 const CrystalList = () => {
     const crystalData = [
-        {
+        {   id: 1,
             nameData: "Amethyst",
             colorData: "Purple",
             powersData: "suuper powers"
     },
-    {
+    {   id: 2,
         nameData: "Jade",
         colorData: "Green",
         powersData: "greeen powers"
     },
-    {
+    {   id: 3,
         nameData: "Opal",
         colorData: "Rainbow",
         powersData: "rainbow powers"
     },
-    {
+    {   id: 4,
         nameData: "Diamond",
         colorData: "transparent",
         powersData: "diamond powers"
+    },
+    {   id: 5,
+        nameData: "Bad Crystal",
+        colorData: "no colors",
+        powersData: "no powers"
     }
 ]
     const crystalComponents = crystalData.map(
         (crystal) => {
             return (
-                <li><Crystal name={crystal.nameData} color={crystal.colorData} powers={crystal.powersData}   /></li>
+                <li key={crystal.id}><Crystal id={crystal.id} name={crystal.nameData} color={crystal.colorData} powers={crystal.powersData}   /></li>
             );
         }
     )
